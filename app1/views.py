9efcfc0eebff5cd292718091f73c76dd31506841
ishamohanty <mohantyisharani@gmail.com>
2023-11-23 13:25:32 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def isha1(request):
+    return render(request,'isha1.html')
+
+def isha2(request):
+    return render(request,'isha2.html')
+
+def isha3(request):
+    return render(request,'isha3.html')
